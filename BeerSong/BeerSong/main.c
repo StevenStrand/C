c5bef656c3f829b4ee394ac543e8b35a13be4950
Steven Strand <steven.strand7@gmail.com>
2015-04-05 14:41:57 -0500

    first commit

---
+++ BeerSong/BeerSong/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  BeerSong
+//
+//  Created by Steven Strand on 4/5/15.
+//  Copyright (c) 2015 Steven Strand. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
